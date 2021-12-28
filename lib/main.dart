7544b6252d8fb5c567d1bd951aec83752348b826
faizza.mohammed <faizza.mohammed@gmail.com>
2021-12-28 22:43:12 +0530

    image loading sample

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Image.asset('assets/images/carambola.jpg'),
+        ),
+      ),
+    ),
+  );
+}
